d6abc7f4a2759851437cd878fe960dfe4c089df8
Filip Kostic <filipkostic@Filips-MacBook-Air.local>
2022-10-11 15:35:19 +0200

    Första Kommentar

--- app/src/main/java/com/example/foodtruck_project/MainActivity.kt
+++ app/src/main/java/com/example/foodtruck_project/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.foodtruck_project
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+
+lateinit var img1 : Image
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,3 +15,5 @@ class MainActivity : AppCompatActivity() {
 }
 
 // filip kommentar
+ 
+
