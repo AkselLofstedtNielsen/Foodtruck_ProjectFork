2b357d66e0654c2332052e84fcfd0b381930a209
Filip Kostic <filipkostic@Filips-MacBook-Air.local>
2022-10-13 10:25:51 +0200

    firestore changes

--- app/src/main/java/com/example/foodtruck_project/SignUpActivity.kt
+++ app/src/main/java/com/example/foodtruck_project/SignUpActivity.kt
@@ -1,11 +1,69 @@
 package com.example.foodtruck_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.foodtruck_project.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
+
+
+
+
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.textView.setOnClickListener {
+            val intent = Intent(this, SignInActivity:: class.java)
+
+            startActivity(intent)
+        }
+        binding.button.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+            val confirmPass = binding.confirmPassEt.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+
+                if (pass == confirmPass) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, SignInActivity:: class.java)
+
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Pasword does not mach", Toast.LENGTH_SHORT).show()
+                }
+
+
+            } else {
+                Toast.makeText(this, "there are empty fields", Toast.LENGTH_SHORT).show()
+            }
+
+
+
+
+
+        }
+
+
+
+
     }
 }
