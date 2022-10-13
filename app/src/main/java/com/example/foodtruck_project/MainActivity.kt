55eb91c37ffaca18964cc15043a586bbd05aa682
Jonas B <jonas.backas@iths.se>
2022-10-13 16:19:58 +0200

    Testar branch

--- app/src/main/java/com/example/foodtruck_project/MainActivity.kt
+++ app/src/main/java/com/example/foodtruck_project/MainActivity.kt
@@ -1,21 +1,25 @@
 package com.example.foodtruck_project
 
-import android.media.Image
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 
-lateinit var img1 : Image
+
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Elin kommentar
+
+
     }
 }
 
-// filip kommentar
+
  
 
