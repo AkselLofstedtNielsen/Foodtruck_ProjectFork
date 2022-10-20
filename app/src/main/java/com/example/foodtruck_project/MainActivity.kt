006e3caae90700320152bc662746dc77a32d0dd7
Filip Kostic <filipkostic@Filips-MacBook-Air.local>
2022-10-20 15:40:02 +0200

    log in firestore

--- app/src/main/java/com/example/foodtruck_project/MainActivity.kt
+++ app/src/main/java/com/example/foodtruck_project/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.foodtruck_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
+
+
+    lateinit var button678 : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        button678 = findViewById(R.id.button678)
+
+
+        button678.setOnClickListener {
+            val intent = Intent(this, SignUpActivity:: class.java)
+            startActivity(intent)
+        }
+
+
+
 
   }
 }
--- app/src/main/java/com/example/foodtruck_project/SignInActivity.kt
+++ app/src/main/java/com/example/foodtruck_project/SignInActivity.kt
@@ -42,7 +42,7 @@ class SignInActivity : AppCompatActivity() {
 
                 firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
                     if (it.isSuccessful) {
-                        val intent = Intent(this, MainActivity:: class.java)
+                        val intent = Intent(this, Item:: class.java)
 
                         startActivity(intent)
                     } else {
