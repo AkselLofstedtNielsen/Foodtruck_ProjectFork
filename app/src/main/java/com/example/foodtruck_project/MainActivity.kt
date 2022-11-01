dd72a4dfa86c508f7e8863b9b7075b1398fa35e6
Jonas B <jonas.backas@iths.se>
2022-11-01 12:57:45 +0100

    La till Användaren får permission att använda gps samt location

--- app/src/main/java/com/example/foodtruck_project/MainActivity.kt
+++ app/src/main/java/com/example/foodtruck_project/MainActivity.kt
@@ -2,25 +2,56 @@ package com.example.foodtruck_project
 
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.example.foodtruck_project.fragments.*
+import com.google.android.gms.location.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val mapexploreFragment = MapexploreFragment()
-    private val searchprefFragment = SearchprefFragment()
-    private val AccProfileFragment = AccProfileFragment()
+    private val REQUEST_LOCATION = 1
     lateinit var navigationMenu: BottomNavigationView
-
+    lateinit var locationProvider : FusedLocationProviderClient
     private val GoogleMapsFragment = GoogleMapsFragment()
+    lateinit var locationCallback: LocationCallback
+    lateinit var locationRequest: LocationRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        locationProvider = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = createLocationRequest()
+       locationCallback = object : LocationCallback() {
+           override fun onLocationResult(locationResult: LocationResult) {
+               for (location in locationResult.locations) {
+                   Log.d("!!!","lat: ${location.latitude}, lng: ${location.longitude}")
+               }
+           }
+       }
+        //locationProvider.requestLocationUpdates(locationRequest,locationCallback, Looper.getMainLooper())
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION
+                )
+
+
+        } else {
+
+        }
+
+
 
         navigationMenu = findViewById(R.id.bottom_navigation)
         replaceFragment(GoogleMapsFragment)
@@ -52,10 +83,41 @@ class MainActivity : AppCompatActivity() {
  */
 
     }
+    fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            locationProvider.requestLocationUpdates(locationRequest,locationCallback, Looper.getMainLooper())
+        }
+
+        }
+
+
+        fun createLocationRequest() =
+        LocationRequest.create().apply {
+        interval = 2000
+        fastestInterval = 1000
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
 
     private fun replaceFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragment_container, fragment)
         transaction.commit()
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_LOCATION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                startLocationUpdates()
+            }
+        }
+
+
+    }
+
 }
