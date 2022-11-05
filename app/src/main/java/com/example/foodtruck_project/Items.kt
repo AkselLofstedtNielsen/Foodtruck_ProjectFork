3fbdc5b83456125196b554804ef3fb2175aa95e4
Elin A <elina@Elins-MacBook-Air.local>
2022-11-05 21:51:07 +0100

    få information om 'food trucks' från databasen på 'food info' sektionen

--- app/src/main/java/com/example/foodtruck_project/Items.kt
+++ app/src/main/java/com/example/foodtruck_project/Items.kt
@@ -2,14 +2,18 @@ package com.example.foodtruck_project
 
 import com.google.firebase.firestore.DocumentId
 
-data class items (@DocumentId var DocumentId : String? = null,
+data class Items (@DocumentId var DocumentId : String? = null,
                   var UnitID : String? = null,
                   var name: String? = null,
                   var openHours: String? = null,
                   var latitude: Double = 0.0,
                   var longitude: Double = 0.0,
-                  var category: String,
+                  var category: String = "",
                   var num: Int? = null,
-                  var truck : Boolean = false)
+                  var truck : Boolean? = false,
+                  var menu: String? = null
+) {
+    constructor(): this(null)
+}
 
 
