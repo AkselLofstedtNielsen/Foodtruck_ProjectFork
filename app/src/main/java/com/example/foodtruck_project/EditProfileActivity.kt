41493cfe80673513b4e3879c6d587f5a6296c25b
Jonas B <jonas.backas@iths.se>
2022-11-07 17:03:34 +0100

    Allting klart, ändrade lite profile osv

--- app/src/main/java/com/example/foodtruck_project/EditProfileActivity.kt
+++ app/src/main/java/com/example/foodtruck_project/EditProfileActivity.kt
@@ -3,13 +3,6 @@ package com.example.foodtruck_project
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.Toast
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
